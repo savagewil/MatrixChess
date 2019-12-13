f8874eb62d316090788f1d1801f6ec10db3abfe7
savagewil <willsvg@gmail.com>
2019-12-13 00:10:10 -0500

    Going to need to draft a model of how it will work

--- MatrixBoard.py
+++ MatrixBoard.py
@@ -1,2 +1,7 @@
 class MatrixBoard:
-    def
+    def __init__(self, board_length, board_width, total_pieces, move_rules):
+        self.move_rules = move_rules
+        self.board_width = board_width
+        self.board_length = board_length
+        self.total_pieces = total_pieces 
+        self.board
